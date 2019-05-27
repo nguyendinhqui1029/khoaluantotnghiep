2aea61025794964ca4b646d23d648f8693eacc0d
NguyenQui <nguyendinhqui1029@gmail.com>
2019-05-28 01:17:39 +0700

    28/05/2019

--- khoaluantotnghiep/src/app/service/config.service.ts
+++ khoaluantotnghiep/src/app/service/config.service.ts
@@ -4,8 +4,8 @@ import { FLAGS } from '@angular/core/src/render3/interfaces/view';
 
 @Injectable()
 export class ConfigService {
-    static URL: string = "https://serverkhoaluan2019.herokuapp.com/";
-    //static URL: string = "http://localhost:8081/";
+    // static URL: string = "https://serverkhoaluan2019.herokuapp.com/";
+    static URL: string = "http://192.168.5.103:8081/";
     static LOAI_TAI_KHOAN: any = { "CUSTOMER": 1, "EMPLOYEE": 2, "ADMIN": 3 };
     static TRANG_THAI_DU_AN: any = { "TATCADUAN": 0, "CHUAGIAODICH": 1, "DANGGIAODICH": 2, "DUANMOI": 3, "DAGIAODICH": 4 };
     static TRANG_THAI_TIN_TUC: any = { "TATCATINTUC": 0, "CHODUYET": 'chờ duyệt', "NEW": 'new', "PHOBIEN": 'phổ biến' };
@@ -17,6 +17,6 @@ export class ConfigService {
     static TRANG_THAI_LOAIGIAODICH = { "TATCA": 0 };
     static LOAI_TIN_TUC: any = { "TATCA": 0 };
     static TRANG_THAI_DOITAC: any = { "TATCA": 0, "CHODUYET": 1, "DADUYET": 2 };
-    static PHI_DANG_BAI:number = 20000;
+    static PHI_DANG_BAI: number = 20000;
 
 }
